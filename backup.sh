2aaf10355f5e53291420a7ea71ea9c95a40580cf
xiongsyao <xiongshengyao@hotmail.com>
2018-12-28 19:05:14 +0800

    fix sh

--- backup.sh
+++ backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo -e "\033[0;32mBacking up source data to GitHub...\033[0m"
+echo -e "\033[0;32mBackup source data to GitHub...\033[0m"
 
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="Baking up `date`"
+msg="backup `date`"
 if [ $# -ne 0 ]
   then msg="$*"
 fi
@@ -15,4 +15,4 @@ git commit -m "$msg"
 #Push source
 git push origin master
 
-echo -e "\033[0;32mFinish backing up!\033[0m"
+echo -e "\033[0;32mFinish backup!\033[0m"
